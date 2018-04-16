625ed5d1e2353fe7a072999452b3109bc6436163
wowoinhfut <l_wowo@sina.com>
2018-04-16 15:40:12 +0800

    add ie9-bellow support

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,15 +2,24 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.alert = this.alert.bind(this);
+  }
+
+  alert() {
+    alert(this.props);
+  }
+
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <Button onClick={this.alert} >hello</Button>
       </div>
     );
   }
